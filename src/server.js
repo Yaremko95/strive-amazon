3e671b2d8a7562052ee883b13b82be257a21ffeb
tetiana <tetianayaremko@gmail.com>
2020-07-02 15:11:39 +0200

    swagger documentation

--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const productRoutes = require("./products");
+const { join } = require("path");
 const reviewsRoutes = require("./reviews");
 const listEndpoints = require("express-list-endpoints");
 const cors = require("cors");
+const YAML = require("yamljs");
+const swaggerUI = require("swagger-ui-express");
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
 const port = process.env.PORT || 3002;
+
+const swaggerDocument = YAML.load(join(__dirname, "./apiDocumentation.yml"));
 
 app.use("/products", productRoutes);
 // app.use('reviews', reviewsRoutes)
@@ -23,6 +28,7 @@ app.use((error, request, response, next) => {
   }
 });
 console.log(listEndpoints(app));
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
